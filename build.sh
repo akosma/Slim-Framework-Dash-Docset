e1d26895b3bd504f105ef3a51e72c0cdf0eeaf30
Adrian Kosmaczewski <ako@trifork.com>
2014-05-22 11:28:46 +0200

    Added an icon to the docset bundle and refactored script

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 
+DEPLOY_PATH="~/Library/Application Support/Dash/DocSets/Slim Framework"
+FILE_NAME="Slim Framework.docset"
+
 # Clean the previous run of this script
 echo "Cleaning"
-rm -r Slim\ Framework.docset/Contents/Resources/Documents/
-mkdir -p Slim\ Framework.docset/Contents/Resources/Documents/
+rm -r "$FILE_NAME/Contents/Resources/Documents/"
+mkdir -p "$FILE_NAME/Contents/Resources/Documents/"
 
 # Download the latest version of the Slim Framework documentation
 echo "Downloading"
@@ -11,17 +14,18 @@ echo "Downloading"
 
 # Put everything in place
 echo "Setting up bundle"
-cp index.webarchive Slim\ Framework.docset/Contents/Resources/Documents/
-cp Info.plist Slim\ Framework.docset/Contents
+cp index.webarchive "$FILE_NAME/Contents/Resources/Documents/"
+cp Info.plist "$FILE_NAME/Contents"
+cp icon.png "$FILE_NAME"
 python ./sqlite.py
 
 # Deploy the documentation in its official location
 echo "Deploying"
-rm -r ~/Library/Application\ Support/Dash/DocSets/Slim\ Framework
-mkdir -p ~/Library/Application\ Support/Dash/DocSets/Slim\ Framework
-cp -r Slim\ Framework.docset ~/Library/Application\ Support/Dash/DocSets/Slim\ Framework
+rm -r "$DEPLOY_PATH"
+mkdir -p "$DEPLOY_PATH"
+cp -r "$FILE_NAME" "$DEPLOY_PATH"
 
-open ~/Library/Application\ Support/Dash/DocSets/Slim\ Framework
+open "$DEPLOY_PATH"
 
 echo "Documentation created. You should double-click on the"
 echo "Slim Framework.docset file to add it to Dash."
